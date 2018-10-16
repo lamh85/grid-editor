d49adbd5a0d94744901fa1b5b7af5605bbc1aac8
Hon Lam <hon@junglescout.com>
2018-10-15 19:47:39 -0700

    Add CSS height editor.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,33 @@ import styled from 'styled-components'
 
 const Styles = styled.div`
   background: red;
-  height: 200px;
+  height: ${props => (props.height + 200) + "px"};
   width: 200px;
 `
 
 class App extends Component {
   constructor(props) {
     super(props)
+    
+    this.state = {
+      height: 0
+    }
+    
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  handleClick() {
+    this.setState({height: this.state.height + 50})
   }
 
   render() {
     return (
-      <Styles>
+      <Styles height={this.state.height}>
         <div>
           Hello world
         </div>
+        
+        <button onClick={this.handleClick}>Click me!</button>
       </Styles>
     )
   }
